dcd3e94c08d74023a90a99ccdce3a94d9f7bdf00
Dimitris Platis <platisd@users.noreply.github.com>
2019-02-17 22:31:08 +0100

    Add IoTink Arduino sketch

---
+++ firmware/IoTink/credentials.h
@@ -0,0 +1,5 @@
+#if NO_CREDENTIALS_HEADER == false
+const auto kInternetSSID = "Honeypott";
+const auto kWifiPassword = "krukuschFR0NG69382!?";
+const auto kServerUrl = "http://platisd.pythonanywhere.com/info-center-data";
+#endif
